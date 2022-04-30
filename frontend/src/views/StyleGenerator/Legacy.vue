1790bad0d57b75b4220ec6f0168cbc203a3a9ae9
DoodleBears <yangmufeng233@gmail.com>
2022-04-30 16:45:15 +0800

    fix: 对齐小表情inline的效果

--- frontend/src/views/StyleGenerator/Legacy.vue
+++ frontend/src/views/StyleGenerator/Legacy.vue
@@ -697,6 +697,8 @@ yt-live-chat-text-message-renderer #image-and-message .emoji {
 }
 
 #image-and-message img[display="inline"] {
+  position: relative;
+  top: 3px;
   border-radius: ${this.form.emoticonInlineBorderRadius}px;
 }`
     },
